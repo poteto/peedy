f942e8af797bf5aab80640ef18713c9a2c9a7d99
Lauren Tan <laurent@netflix.com>
2017-05-11 23:05:36 -0700

    Bump processor interval

--- apps/queue/lib/pipeline/processor.ex
+++ apps/queue/lib/pipeline/processor.ex
@@ -1,7 +1,7 @@
 defmodule Queue.Pipeline.Processor do
   require Logger
 
-  @interval 1_000
+  @interval 5_000
 
   def start_link(message) do
     {:ok, _} = Task.start_link(__MODULE__, :process_message, [message])
